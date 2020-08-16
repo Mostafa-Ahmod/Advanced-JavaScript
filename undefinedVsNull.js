096f8719715167b759c941dc3f2fcba9581c1355
Mostafa Ahmod Sajid <64971962+Mostafa-Ahmod@users.noreply.github.com>
2020-08-16 14:13:20 +0600

    null vs defined

---
+++ undefinedVsNull.js
@@ -0,0 +1,18 @@
+// // let bird ;
+// function add(num1,num2){
+//      console.log(num1 + num2);
+//      return
+// }
+
+// function add(num1,num2=10) {
+//     console.log(num1,num2);
+// }
+// const result = add(15);
+// console.log(result);
+
+const lover = {name:'Beautiful girl', phone: 53989};
+console.log(lover.gf);
+
+let fun = undefined;
+console.log(fun);
+
