b03ca75a3081e181c6647a29f3066652ed76ff40
Mostafa Ahmod Sajid <64971962+Mostafa-Ahmod@users.noreply.github.com>
2020-08-16 13:36:45 +0600

    trueflase

---
+++ trueAndFalse.js
@@ -0,0 +1,29 @@
+// False
+// 0
+// ""
+// NaN
+// null
+// undefined
+
+
+
+// const age = 5;
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+// Truthy
+// '0' , ' ',[],{},
+
+// const name = "0";
+let name = 'false';
+console.log(name);
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
